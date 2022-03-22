39c3fac93cfab1ffe99c0535d10649d69e3b7ab8
Printed Love <dnjswn0328@gmail.com>
2022-03-22 11:10:00 +0900

    2022/03/22

--- and_lec/chap02/VisualTool 2(Lab)/app/src/main/java/kr/co/company/visualtool2lab/MainActivity.java
+++ and_lec/chap02/VisualTool 2(Lab)/app/src/main/java/kr/co/company/visualtool2lab/MainActivity.java
@@ -14,8 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void onClicked(View v) {
+    public void onClicked2(View v) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1234-5678"));
         startActivity(intent);
     }
 }
+/*
+    인텐트 : 컴포넌트를 실행하기 위해 시스템에 넘기는 정보
+
+    실행하고자 하는 컴포넌트 정보를 담은 Intent 구성 -> 시스템 -> Intent 정보를 통해 그에 맞는 컴포넌트를 실행.
+ */
